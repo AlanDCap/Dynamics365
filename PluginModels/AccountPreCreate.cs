ffa76a48276b7863cad43e247def8b7ec6e59579
Alan Capelari <alandc@hotmail.com.br>
2024-12-02 20:08:30 -0300

    Author: Alan Capelari Date: Reason: Adição de projeto base para desenvolvimento de plugins

---
+++ PluginModels/AccountPreCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.ServiceModel;
+
+namespace PluginModels
+{
+    public class AccountPreCreate : IPlugin
+    {
+        public AccountPreCreate() { }
+        public AccountPreCreate(string unsecure) { }
+        public AccountPreCreate(string unsecure, string secure) { }
+
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            ITracingService tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            try
+            {
+                //logic goes here
+            }
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                throw new InvalidPluginExecutionException("Erro ao executar o plugin AccountPreCreate.", ex);
+            }
+            catch (Exception ex)
+            {
+                tracing.Trace("AccountPreCreate: erro: {0}", ex.ToString());
+            }
+        }
+    }
+}
